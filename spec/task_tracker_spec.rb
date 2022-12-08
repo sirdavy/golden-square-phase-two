28757276acebb80d3ca84f60e6c1b972167cedae
David Scott <sirdavidscott@hotmail.com>
2022-12-08 16:42:41 +0000

    task_tracker 2nd commit

--- spec/task_tracker_spec.rb
+++ spec/task_tracker_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "task_tracker method" do
     result = task_tracker("TODO - clean your room.")
     expect(result).to eq true
   end
+  it "returns false for string not containing TODO" do
+    result = task_tracker("Wash the cat.")
+    expect(result).to eq false
+  end
+
 end
