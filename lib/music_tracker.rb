c485b7052dec2f75d57f42305f7170c662538327
David Scott <sirdavidscott@hotmail.com>
2022-12-13 16:24:39 +0000

    music_tracker final commit

--- lib/music_tracker.rb
+++ lib/music_tracker.rb
@@ -1,15 +1,21 @@
 class MusicTracker
-  
   def initialize
     @music_list = []
   end
 
   def add(text)
-    @music_list << text
+    if @music_list.include?(text)
+      fail "Track already added."
+    else
+      @music_list << text
+    end
   end
 
   def view
-    return @music_list
+    if @music_list.empty?
+      fail "List is empty"
+    else
+      return @music_list
+    end
   end
-  
 end
--- spec/music_tracker_spec.rb
+++ spec/music_tracker_spec.rb
@@ -1,4 +1,4 @@
-require 'music_tracker'
+require "music_tracker"
 
 RSpec.describe MusicTracker do
   it "adds a track to the list" do
@@ -6,24 +6,24 @@ RSpec.describe MusicTracker do
     playlist.add("Kid Charlemagne")
     expect(playlist.view).to eq ["Kid Charlemagne"]
   end
+
+  it "add more than one track to list" do
+    playlist = MusicTracker.new
+    playlist.add("Kid Charlemagne")
+    playlist.add("Hey, Nineteen")
+    playlist.view => ["Kid Charlemagne", "Hey, Nineteen"]
+  end
+
+  it "returns error if track is already listed" do
+    playlist = MusicTracker.new
+    playlist.add("Babylon Sisters")
+    expect {
+      playlist.add("Babylon Sisters")
+    }.to raise_error "Track already added."
+  end
+
+  it "returns error if list is empty" do
+    playlist = MusicTracker.new
+    expect { playlist.view }.to raise_error "List is empty"
+  end
 end
-
-# # 1
-# playlist = MusicTracker.new
-# playlist.add("Kid Charlemagne")
-# playlist.view => ["Kid Charlemagne"]
-
-# # 2
-# playlist = MusicTracker.new
-# playlist.add("Kid Charlemagne")
-# playlist.add("Hey, Nineteen")
-# playlist.view => ["Kid Charlemagne", "Hey, Nineteen"]
-
-# # 3
-# playlist = MusicTracker.new
-# playlist.add("Babylon Sisters")
-# playlist.add("Babylon Sisters") => "Track already added."
-
-# # 4
-# playlist = MusicTracker.new
-# playlist.view => "List is empty"
